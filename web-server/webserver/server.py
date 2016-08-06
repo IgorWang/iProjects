8926bf354b5b122d799134fea501f259e856056a
igor <125942418@qq.com>
2016-08-06 14:58:45 +0800

    add exception handel

--- web-server/webserver/server.py
+++ web-server/webserver/server.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 # Project : web-server
 # Created by igor on 16/8/6
-import BaseHTTPServer
+import BaseHTTPServer, os, sys
+
+
+class ServerException(Exception):
+    '''
+    服务器内部错误
+    '''
+    pass
 
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
@@ -9,20 +16,78 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     # 页面模版
     Page = '''
-            <html>
-            <body>
-            <p>Hello, web!</p>
-            </body>
-            </html>
-            '''
+    <html>
+    <body>
+    <table>
+    <tr> <td>Header</td>        <td><Value></td>        </tr>
+    <tr> <td>Date and time</td> <td>{date_time}</td>    </tr>
+    <tr> <td>Client host</td>   <td>{client_host}</td>  </tr>
+    <tr> <td>Client port</td>   <td>{client_port}</td>  </tr>
+    <tr> <td>Command</td>       <td>{command}</td>      </tr>
+    <tr> <td>Path</td>          <td>{path}</td>         </tr>
+    </table>
+    </body>
+    </html>
+    '''
+
+    # 错误页
+    Error_Page = '''
+    <html>
+    <body>
+    <h1>Error accessing {path}</h1>
+    <p>{msg}</p>
+    </body>
+    </html>
+    '''
 
     # 处理一个GET请求
     def do_GET(self):
-        self.send_response(200)  # 状态码200,表示请求成功响应
-        self.send_header("Content-Type", 'text/html')
-        self.send_header("Content-Length", str(len(self.Page)))
+        try:
+            full_path = os.getcwd() + self.path
+            if not os.path.exists(full_path):
+                raise ServerException("'{0} not found ".format(self.path))
+
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+            else:
+                raise ServerException("Unkonw object '{0}'".format(self.path))
+
+        except Exception as msg:
+            self.handle_error(msg)
+        page = self.create_page()
+        self.send_content(page)
+
+    def create_page(self):
+        values = {
+            'date_time': self.date_time_string(),
+            'client_host': self.client_address[0],
+            'client_port': self.client_address[1],
+            'command': self.command,
+            'path': self.path
+        }
+        page = self.Page.format(**values)
+        return page
+
+    def send_content(self, page, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(len(page)))
         self.end_headers()
-        self.wfile.write(self.Page)
+        self.wfile.write(page)
+
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read : {1}".format(self.path, msg)
+            self.handle_error(msg)
+
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path,
+                                         msg=msg)
+        self.send_content(content, status=404)
 
 
 if __name__ == "__main__":
